e0fe42667670074f3c2933f2d76e976099a0201b
Mikael Ramo <Mikaelhr@metropolia.fi>
2017-11-29 01:26:40 +0200

    Added Where to start page and js file + fixed couple small bugs

---
+++ web/js.js
@@ -0,0 +1,33 @@
+var startBox2 = document.getElementById("start-box-2");
+var startBox1 = document.getElementById("start-box-1");
+var button1 = document.getElementById("sheet-1");
+var button2 = document.getElementById("sheet-2");
+var button = document.getElementById("piano");
+
+
+
+button.onclick = function () {
+    if (startBox2.className !== "piano"){
+        startBox2.className = "piano";
+        startBox1.innerHTML = "Piano keyboard";
+        startBox2.innerHTML = " Something here. <br> Div id = start-box-2 <br>Viewing piano NOW";
+    }
+}
+
+
+button1.onclick = function () {
+    if (startBox2.className !== "sheet-1"){
+        startBox2.className = "sheet-1";
+        startBox1.innerHTML = "Music sheet 1";
+        startBox2.innerHTML = " Something here. <br> Div id = start-box-2 <br>Viewing sheet 1 NOW";
+    }
+}
+
+
+button2.onclick = function () {
+    if (startBox2.className !== "sheet-2"){
+        startBox2.className = "sheet-2";
+        startBox1.innerHTML = "Music sheet 2";
+        startBox2.innerHTML = " Something here. <br> Div id = start-box-2 <br> Viewing sheet 2 NOW";
+    }
+}
