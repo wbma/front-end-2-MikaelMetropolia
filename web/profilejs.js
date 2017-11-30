9812894c715543d29da8a85f44b198c45011400c
MikaelMetropolia <Mikaelhr@metropolia.fi>
2017-12-01 01:48:43 +0200

    Added all difficulties, profile settings and added some more javascript

--- web/profilejs.js
+++ web/profilejs.js
@@ -1,118 +1,106 @@
-var element = document.getElementById('arrow1'),
+const element = document.getElementById('arrow1'),
 style = window.getComputedStyle(element),
 right1 = style.getPropertyValue('transform');
 
-var element2 = document.getElementById('arrow2'),
+const element2 = document.getElementById('arrow2'),
 style2 = window.getComputedStyle(element2),
 right2 = style.getPropertyValue('transform');
 
-var element3 = document.getElementById('arrow3'),
+const element3 = document.getElementById('arrow3'),
 style3 = window.getComputedStyle(element3),
 right3 = style.getPropertyValue('transform');
 
-var element4 = document.getElementById('arrow4'),
+const element4 = document.getElementById('arrow4'),
 style4 = window.getComputedStyle(element4),
 right4 = style.getPropertyValue('transform');
 
-var dropElement = document.getElementById('dropdown');
-var dropElement2 = document.getElementById('dropdown2');
-var dropElement3 = document.getElementById('dropdown3');
-var dropElement4 = document.getElementById('dropdown4');
-var myElement = document.querySelector("#pepe-menu-id");
+const dropElement = document.getElementById('dropdown');
+const dropElement2 = document.getElementById('dropdown2');
+const dropElement3 = document.getElementById('dropdown3');
+const dropElement4 = document.getElementById('dropdown4');
+const myElement = document.querySelector("#pepe-menu-id");
 
-var button3 = document.getElementById("uploads");
+const button3 = document.getElementById("uploads");
 const button4 = document.getElementById("uploads2");
-var button5 = document.getElementById("uploads3");
-var button6 = document.getElementById("uploads4");
-var pepeButton = document.getElementById("pepe");
+const button5 = document.getElementById("uploads3");
+const button6 = document.getElementById("uploads4");
+const pepeButton = document.getElementById("pepe");
 
 myElement.style.display = 'none';
 
-const buttonElement = document.getElementById("test-button");
-const compList = document.getElementById('image-wrapper');
-
-buttonElement.addEventListener('click', () => {
-    addNewComposition ();
-}
+button3.addEventListener('click', () => {
+    hideMenu(right1, element, dropElement);
 });
-
-const addNewComposition = () => {
-compList.innerHTML += ' <div class="composition">
-                               <div class="play-comp-title">Anh. 115 Minuet <br> J.S.Bach </div>
-                               <div class="play-comp-image"><img src="play-button.png"></div>
-                           </div>
-                       </div> ';
-
-}
-const piilotaValikko =  (nuoli, element, dropElement) => {
-    if (nuoli == 'rotate(-45deg)' || nuoli == 'matrix(0.707107, -0.707107, 0.707107, 0.707107, 0, 0)' ) {
-        element.style.transform = "rotate(45deg)";
-        dropElement.style.display = "inline";
-        nuoli = 'rotate(45deg)';
-        console.log('Arrow down');
-    } else {
-        element.style.transform = "rotate(-45deg)";
-        dropElement.style.display = "none";
-        nuoli = 'rotate(-45deg)';
-        console.log('Arrow up');
-    }
-}
-
-button5.addEventListener('click', () => {
-    piilotaValikko(right3, element3, dropElement3);
-});
-
-
-
 button4.addEventListener('click', () => {
-    if (right2 == 'rotate(-45deg)' || right2 == 'matrix(0.707107, -0.707107, 0.707107, 0.707107, 0, 0)' ) {
-        element2.style.transform = "rotate(45deg)";
-        dropElement2.style.display = "inline";
-        right2 = 'rotate(45deg)';
-        console.log('Arrow down');
-    } else {
-        element2.style.transform = "rotate(-45deg)";
-        dropElement2.style.display = "none";
-        right2 = 'rotate(-45deg)';
-        console.log('Arrow up');
-    }
+    hideMenu(right2, element2, dropElement2);
+});
+button5.addEventListener('click', () => {
+    hideMenu(right3, element3, dropElement3);
+});
+button6.addEventListener('click', () => {
+    hideMenu(right4, element4, dropElement4);
 });
 
+/*
+hideMenu(right4, element4, dropElement4);
 
+var arrowState1 = 0;
+var arrowState2 = 0;
+var arrowState3 = 0;
+var arrowState4 = 0;
 
-button3.onclick = function () {
-    if (right1 == 'rotate(-45deg)' || right1 == 'matrix(0.707107, -0.707107, 0.707107, 0.707107, 0, 0)' ) {
+const hideMenu =  (arrow, element, dropElement, arrowState) => {
+    console.log(arrowState);
+    if (arrowState === 0) {
         element.style.transform = "rotate(45deg)";
         dropElement.style.display = "inline";
-        right1 = 'rotate(45deg)';
+        arrowState++;
         console.log('Arrow down');
+        console.log(arrowState);
     } else {
         element.style.transform = "rotate(-45deg)";
         dropElement.style.display = "none";
-        right1 = 'rotate(-45deg)';
+        arrowState--;
+        arrow = 'rotate(-45deg)';
         console.log('Arrow up');
+        console.log(arrowState);
     }
-}
-
-
-button6.onclick = function () {
-    if (right4 == 'rotate(-45deg)' || right3 == 'matrix(0.707107, -0.707107, 0.707107, 0.707107, 0, 0)' ) {
-        element4.style.transform = "rotate(45deg)";
-        dropElement4.style.display = "inline";
-        right4 = 'rotate(45deg)';
-        console.log('Arrow down');
-    } else {
-        element4.style.transform = "rotate(-45deg)";
-        dropElement4.style.display = "none";
-        right4 = 'rotate(-45deg)';
-        console.log('Arrow up');
-    }
-}
-
+    console.log(arrowState);
+};
+*/
 pepeButton.onclick = function () {
     if (myElement.style.display == 'none') {
         myElement.style.display = 'inline';
   } else {
      myElement.style.display = 'none';
     }
-}
+};
+
+
+
+const hideMenu =  (arrow, element, dropElement) => {
+console.log(arrow);
+    if (arrow == 'rotate(-45deg)' || arrow == 'matrix(0.707107, -0.707107, 0.707107, 0.707107, 0, 0)') {
+        element.style.transform = "rotate(45deg)";
+        arrow = 'rotate (45deg)';
+        dropElement.style.display = "inline";
+        console.log('Arrow down');
+        console.log(arrow);
+    } else {
+        element.style.transform = "rotate(-45deg)";
+        dropElement.style.display = "none";
+        arrow = 'rotate(-45deg)';
+        console.log('Arrow up');
+        console.log(arrow);
+    }
+    console.log(arrow);
+};
+
+
+
+
+
+
+
+
+
---
+++ web/settings.js
@@ -0,0 +1,33 @@
+const myElement = document.querySelector("#pepe-menu-id");
+const pepeButton = document.getElementById("pepe");
+const pepeProfileImg = document.getElementById("profile-img");
+const pepeSquareImg = document.getElementById("pepe");
+const removeButton = document.getElementById("remove-photo");
+const addButton = document.getElementById("add-photo");
+
+myElement.style.display = 'none';
+
+pepeButton.onclick = function () {
+    if (myElement.style.display == 'none') {
+        myElement.style.display = 'inline';
+  } else {
+     myElement.style.display = 'none';
+    }
+};
+
+removeButton.addEventListener('click', () => {
+    pepeProfileImg.src = "default.jpg";
+    pepeSquareImg.src = "default.jpg";
+});
+
+addButton.addEventListener('click', () => {
+    pepeProfileImg.src = "pepe.png";
+    pepeSquareImg.src = "pepe.png";
+});
+
+
+
+
+
+
+
